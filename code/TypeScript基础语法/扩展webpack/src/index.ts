a2ea3d18fcfa2e1ce18c279d94fab09852eea6e3
AndyLi-Coder <1659815198@qq.com>
2023-03-08 22:16:34 +0800

    webpack环境的搭建

---
+++ code/TypeScript基础语法/扩展webpack/src/index.ts
@@ -0,0 +1,7 @@
+
+import { num } from './utils/math'
+const message:string = 'hello world'
+
+
+console.log(message);
+console.log(num(10,20))
---
+++ code/TypeScript基础语法/扩展webpack/src/utils/math.ts
@@ -0,0 +1,3 @@
+export function num(num1: number,num2: number) {
+    return num1 + num2
+}
