1d21bf3cf94d6b042e861fb25dfafc3b89d70e1b
AndyLi-Coder <1659815198@qq.com>
2023-03-08 22:29:23 +0800

    1

--- code/TypeScript基础语法/扩展webpack/src/index.ts
+++ code/TypeScript基础语法/扩展webpack/src/index.ts
@@ -5,3 +5,11 @@ const message:string = 'hello world'
 
 console.log(message);
 console.log(num(10,20))
+
+
+
+// lib.dom.d.ts
+
+const h2El = document.createElement('h2')
+h2El.textContent = 'Hello TypeScript'
+document.body.append(h2El)
